6f64bea8bb2cb3a1e116521267b15d6b7b8d6f46
Гадышкины <grek-prost@yandex.ru>
2022-09-29 23:15:06 +0900

    2. Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -19,4 +19,24 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public void printInfo() {
+        System.out.println(id);
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.printInfo();
+        System.out.println();
+        Item item1 = new Item();
+        item1.setName("Anatolii Gady");
+        item1.printInfo();
+        System.out.println();
+        Item item2 = new Item();
+        item2.setId(32);
+        item2.setName("Anatolii Gady");
+        item2.printInfo();
+
+    }
 }
