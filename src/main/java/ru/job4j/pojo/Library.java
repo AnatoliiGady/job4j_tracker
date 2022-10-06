97c7c93cb2b9ed9f4c3ba42307eddaf31bc7f861
Гадышкины <grek-prost@yandex.ru>
2022-10-06 21:44:33 +0900

    2. Модель данных.

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -25,7 +25,7 @@ public class Library {
         }
         System.out.println();
         for (Book bk : books) {
-            if (bk.getName().equals("Clean code")) {
+            if (("Clean code").equals(bk.getName())) {
                 System.out.println(bk.getName() + "." + " Количество страниц: " + bk.getCountPage());
             }
         }
