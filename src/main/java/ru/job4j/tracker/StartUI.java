342730f3134399c1f737c0fbb18bdc643345795c
Гадышкины <grek-prost@yandex.ru>
2022-10-22 22:21:09 +0900

    2.3. Реализация класса StartUI. Добавление заявки

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,6 +9,14 @@ public class StartUI {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            }
             if (select != 6) {
                 System.out.println("Пользователь выбрал: " + select);
             } else {
