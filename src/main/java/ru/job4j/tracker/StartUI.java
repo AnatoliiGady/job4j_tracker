780741b0203bc036a11c6545e0a535fa3c9b9d95
Гадышкины <grek-prost@yandex.ru>
2022-10-24 16:35:15 +0900

    2.8. Реализация класса StartUI. Вывод заявок по имени

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -57,7 +57,19 @@ public class StartUI {
                 } else {
                     System.out.println("Заявка с веденным id: " + id + " не найдена.");
                 }
-                if (select != 6) {
+            } else if (select == 5) {
+                System.out.println("=== Find items by name ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
+                }
+            if (select != 6) {
                     System.out.println("Пользователь выбрал: " + select);
                 }
             } else {
