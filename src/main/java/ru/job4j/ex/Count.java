8ebe664837746f3727f880b75708b90a5eb8e4de
AnatoliiGady <grek-prost@yandex.ru>
2022-11-12 20:06:29 +0900

    0.3. Тестирование исключений с junit

--- src/main/java/ru/job4j/ex/Count.java
+++ src/main/java/ru/job4j/ex/Count.java
@@ -13,6 +13,6 @@ public class Count {
     }
 
     public static void main(String[] args) {
-        System.out.println("Sum of 2 to 10 is: " + add(11, 10));
+        System.out.println("Sum of 2 to 10 is: " + add(1, 10));
     }
 }
---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CountTest {
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Count.add(10, 2);
+                });
+        assertThat(exception.getMessage()).isEqualTo("Start should be less than finish");
+    }
+
+    @Test
+    public void whenStart1Finish10Summa45(){
+        int start = 1;
+        int finish = 10;
+        int expected = 45;
+        int result = Count.add(start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+}
