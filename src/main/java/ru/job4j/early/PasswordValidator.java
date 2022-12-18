ba4c6fa160c3904cf68197325043560abaee8816
AnatoliiGady <grek-prost@yandex.ru>
2022-12-18 19:15:46 +0900

    4. Принципы раннего возврата и охранных выражений

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -2,7 +2,10 @@ package ru.job4j.early;
 
 public class PasswordValidator {
 
+    private static final String[] wrongSubString = {"qwerty", "12345", "password", "admin", "user"};
+
     public static String validate(String password) {
+
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
@@ -18,26 +21,22 @@ public class PasswordValidator {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
         boolean containDigit = false;
+        boolean containSpecialChar = false;
         for (Character ch : password.toCharArray()) {
             if (Character.isDigit(ch)) {
                 containDigit = true;
+            }
+            if (!Character.isLetter(ch) && !Character.isDigit(ch)) {
+                containSpecialChar = true;
                 break;
             }
         }
         if (!containDigit) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
-        boolean containSpecialChar = false;
-        for (Character ch : password.toCharArray()) {
-            if (!Character.isLetter(ch) && !Character.isDigit(ch)) {
-                containSpecialChar = true;
-                break;
-            }
-        }
         if (!containSpecialChar) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
-        String[] wrongSubString = {"qwerty", "12345", "password", "admin", "user"};
         for (String w : wrongSubString) {
             if (password.toLowerCase().contains(w)) {
                 throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
