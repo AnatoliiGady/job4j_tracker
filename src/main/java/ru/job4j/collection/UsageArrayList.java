16be86ecd4140bc6e9ca08f4466e64b6850597e2
AnatoliiGady <grek-prost@yandex.ru>
2023-01-01 18:38:17 +0900

    2. Коллекции, ArrayList, List, Обобщения

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -9,7 +9,7 @@ public class UsageArrayList {
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
-        for (Object value : names) {
+        for (String value : names) {
             System.out.println(value);
         }
     }
