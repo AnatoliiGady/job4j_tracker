558fe7d1a47821585d0cf88a8c6e1d6783c0a019
AnatoliiGady <grek-prost@yandex.ru>
2023-01-13 12:29:30 +0900

    0. Map, HashMap

---
+++ src/main/java/ru/job4j/queue/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.queue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("grek-prost@yandex.ru", "Gady Anatolii Nikolaevich");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
