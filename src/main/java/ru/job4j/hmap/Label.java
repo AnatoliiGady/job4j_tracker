2f67867e7f51de59470648252aad7412558d2615
AnatoliiGady <grek-prost@yandex.ru>
2023-01-27 14:26:19 +0900

    4. Аттестация

--- src/main/java/ru/job4j/hmap/Label.java
+++ src/main/java/ru/job4j/hmap/Label.java
@@ -1,6 +1,6 @@
 package ru.job4j.hmap;
 
-public record Label(String name, double score) implements Comparable<Label> { //содержит результаты: имя и балы. Используется как для учениеов так и для предметов
+public record Label(String name, double score) implements Comparable<Label> {
     @Override
     public int compareTo(Label o) {
         return Double.compare(this.score, o.score);
--- src/main/java/ru/job4j/hmap/Pupil.java
+++ src/main/java/ru/job4j/hmap/Pupil.java
@@ -2,5 +2,5 @@ package ru.job4j.hmap;
 
 import java.util.List;
 
-public record Pupil(String name, List<Subject> subjects) { //класс описывает ученика
+public record Pupil(String name, List<Subject> subjects) {
 }
