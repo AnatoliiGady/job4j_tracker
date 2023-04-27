484657d4e388781f1fd6ee1855e6af23e66b7214
AnatoliiGady <grek-prost@yandex.ru>
2023-04-27 18:57:33 +0900

    Function

--- src/main/java/ru/job4j/fuction/FunctionInterfaceUsage.java
+++ src/main/java/ru/job4j/fuction/FunctionInterfaceUsage.java
@@ -23,20 +23,20 @@ public class FunctionInterfaceUsage {
         System.out.println("Строка содержит под строку: " + cond.test("name123", 123));
         System.out.println("Строка содержит под строку: " + cond.test("name", 123));
 
-        //TODO Function чтение символа с определенным индексом с помощью функционального интерфейса
+
         Function<String, Character> func = s -> s.charAt(2);
         System.out.println("Третий символ в строке: " + func.apply("first"));
         System.out.println("Третий символ в строке: " + func.apply("second"));
-        //TODO Bi форма Function
+
         BiFunction<String, Integer, String> biFunc = (s, index) -> s.concat(" ").concat(index.toString());
         System.out.println("Результат работы бифункции: " + biFunc.apply("name", 123));
         System.out.println("Результат работы бифункции: " + biFunc.apply("String number", 12345));
-        //TODO UnaryOperator и BinaryOperator разновидность Function (входные и выходные параметры должны совподать)
-        //TODO перевернем строку
+
+
         UnaryOperator<StringBuilder> builder = b -> b.reverse();
         System.out.println("Строка после реверса: " + builder.apply(new StringBuilder("String for test")));
         System.out.println("Строка после реверса: " + builder.apply(new StringBuilder("tset rof gnirtS")));
-        //TODO применение BinaryOperator для объеденения двух строк
+
         BinaryOperator<StringBuilder> builders = (b1, b2) -> b1.append(" ").append(b2);
         System.out.println(
                 "Строка после объеденения: " + builders.apply(
